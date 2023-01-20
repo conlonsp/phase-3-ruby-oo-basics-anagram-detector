fd31da9d857ea6e9bf551a5b527c9b9032826314
samuelconlon <conlonsp@yahoo.com>
2023-01-20 13:36:57 -0500

    commit tree 58ea7ae0bc89a4a062a5d907b715966527c790eb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word)
+    word.filter do |w|
+      w.chars.sort == @word.chars.sort
+    end
+  end
+end
